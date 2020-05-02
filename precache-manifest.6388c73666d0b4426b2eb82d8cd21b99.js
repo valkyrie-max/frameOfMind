d256872dc84827e00383d48cecd448500460112e
valkyrie-max <alisakitkina2016@gmail.com>
2020-05-01 19:53:08 -0500

    Updates

---
+++ precache-manifest.6388c73666d0b4426b2eb82d8cd21b99.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9beb055b98147fc56ab8e42102076148",
+    "url": "/frameOfMind/index.html"
+  },
+  {
+    "revision": "e90d9d6ad9e3869853a3",
+    "url": "/frameOfMind/static/css/main.ff4c8873.chunk.css"
+  },
+  {
+    "revision": "f1d3542d900f33d343e0",
+    "url": "/frameOfMind/static/js/2.d7902839.chunk.js"
+  },
+  {
+    "revision": "7585d7e2191ead1c7fd01d7ebe90ade7",
+    "url": "/frameOfMind/static/js/2.d7902839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e90d9d6ad9e3869853a3",
+    "url": "/frameOfMind/static/js/main.447d09ea.chunk.js"
+  },
+  {
+    "revision": "35933cb309fe484e38ae",
+    "url": "/frameOfMind/static/js/runtime-main.2732bde7.js"
+  },
+  {
+    "revision": "f26a93e17b78ed86ef523772d1f60802",
+    "url": "/frameOfMind/static/media/anger.f26a93e1.mp3"
+  },
+  {
+    "revision": "51cbc765d2be6b539cbc28cb084526de",
+    "url": "/frameOfMind/static/media/empty.51cbc765.mp3"
+  },
+  {
+    "revision": "efc9091e6e7dc5461f9bbb0046aca6f6",
+    "url": "/frameOfMind/static/media/joy.efc9091e.mp3"
+  },
+  {
+    "revision": "81c40b7d29d198dc492e55aeddec352b",
+    "url": "/frameOfMind/static/media/projectLogo.81c40b7d.png"
+  },
+  {
+    "revision": "a8e8e5f6e79a055654a1c3ff422be780",
+    "url": "/frameOfMind/static/media/sadness.a8e8e5f6.mp3"
+  },
+  {
+    "revision": "a6d6edc4d05c1ef41f5744411ae76dd6",
+    "url": "/frameOfMind/static/media/tranquil.a6d6edc4.mp3"
+  }
+]);
